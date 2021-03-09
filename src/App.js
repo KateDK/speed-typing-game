0fab2e01916a80561d8d81d403233906b6ced3e4
Kate Dubitski <kate.dubitski@gmail.com>
2021-03-09 16:17:48 -0500

    Initiate state for text area text

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [inputVal, setInputVal] = useState('');
     <div className="App">
       <h1>Title</h1>
       <textarea
