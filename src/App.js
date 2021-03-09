94bbcb7f87442551b5b954df69100f32d8ab4a2e
Kate Dubitski <kate.dubitski@gmail.com>
2021-03-09 16:18:29 -0500

    provide textArea with value and onChange function

--- src/App.js
+++ src/App.js
@@ -3,9 +3,12 @@ import './App.css';
 
 function App() {
   const [inputVal, setInputVal] = useState('');
+  return (
     <div className="App">
       <h1>Title</h1>
       <textarea
+        value={inputVal}
+        onChange={(e) => setInputVal(e.target.value)}
       />
       <h4>Time remaining: </h4>
       <h1>Word count: </h1>
